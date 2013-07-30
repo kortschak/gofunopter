a8c6da7ad9bcb69b3124a75be57978b9cb08d122
btracey <bdtinternet@gmail.com>
2013-07-30 13:21:15 -0700

    Removed all old bad files

--- interfaces.go
+++
@@ -1,5 +0,0 @@
-package gofunopter
-
-type SisoGradFreeUncon interface{
-	
-}
--- simpletypes.go
+++
@@ -1,6 +0,0 @@
-package gofunopter
-
-// Each of the specific settings may not have things that make sense
-// (for example, the location shouldn't have a tolerance) but it's way
-// easier to just have one for each type
-// Specific types can overwrite various parts if desired
